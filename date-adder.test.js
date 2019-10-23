bdf3082cfafffb67a9161d2238bc24f3d4bcd8b9
Phil Fitch <mostreluctantly@gmail.com>
2019-10-23 14:26:38 -0700

    solved without 'moment'

--- date-adder.test.js
+++ date-adder.test.js
@@ -4,10 +4,16 @@ const add = require('./date-adder');
 
 describe('add', () => {
   it('adds given time to a given date', () => {
-    const date = new Date(2019, 10, 21, 18, 0, 0, 0);
-    console.log(date);
+    let date = new Date(2019, 9, 21, 18, 0, 0, 0);
     const diff = '10d';
 
-    expect(add(date, diff)).toEqual(new Date(2019, 10, 31, 18, 0, 0, 0));
+    expect(add(date, diff)).toEqual(new Date(2019, 9, 31, 18, 0, 0, 0));
   });
+
+  it('adds weeks accurately', () => {
+    let date = new Date(2019, 9, 21, 18, 0, 0, 0);
+    const diff = '1w';
+
+    expect(add(date, diff)).toEqual(new Date(2019, 9, 28, 18, 0, 0, 0));
+  })
 });
