4d2df0582d50b7f654b7929d95e19040c887b7c4
Phil Fitch <mostreluctantly@gmail.com>
2019-12-04 13:02:11 -0800

    intersection, union, and difference tests written

--- Set.test.js
+++ Set.test.js
@@ -7,35 +7,44 @@ describe('Set', () => {
 
   it('add', () => {
     let set = new Set;
-    set.add('balloon');
-    expect(set.array).toEqual(['balloon']);
-    set.add('balloon');
-    expect(set.array).toEqual(['balloon']);
+    set.add('balloons');
+    expect(set.array).toEqual(['balloons']);
+    set.add('balloons');
+    expect(set.array).toEqual(['balloons']);
   })
 
   it('remove', () => {
     let set = new Set;
-    set.add('balloon');
-    set.remove('balloon');
+    set.add('balloons');
+    set.remove('balloons');
     expect(set.array).toEqual([]);
   })
 
   it('has', () => {
     let set = new Set;
-    set.add('balloon');
-    expect(set.has('balloon')).toBe(true);
+    set.add('balloons');
+    expect(set.has('balloons')).toBe(true);
   })
 
   it('intersection', () => {
-
+    let set = new Set;
+    set.add('balloons');
+    set.add('clowns');
+    expect(set.intersection(['balloons', 'cake'])).toEqual(['balloons']);
   })
 
   it('union', () => {
-
+    let set = new Set;
+    set.add('balloons');
+    set.add('clowns');
+    expect(set.union(['balloons', 'cake'])).toEqual(['balloons', 'clowns', 'cake']);
   })
 
   it('difference', () => {
-
+    let set = new Set;
+    set.add('balloons');
+    set.add('clowns');
+    expect(set.difference(['balloons', 'cake'])).toEqual(['clowns', 'cake']);
   })
 
   it('static intersection', () => {
