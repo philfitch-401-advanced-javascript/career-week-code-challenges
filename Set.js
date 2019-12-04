5bc349b18d2d1a841e71c67c0abc64a5db6697a0
Phil Fitch <mostreluctantly@gmail.com>
2019-12-04 11:30:40 -0800

    Set-up

---
+++ Set.js
@@ -0,0 +1,23 @@
+class Set {
+  constructor() {[]}
+
+  add() {}
+
+  remove() {}
+
+  has() {}
+
+  intersection() {}
+
+  union() {}
+
+  difference() {}
+
+  static intersection() {}
+
+  static union() {}
+
+  static difference() {}
+}
+
+module.exports = { Set };
---
+++ Set.test.js
@@ -0,0 +1,41 @@
+
+const Set = require('./Set');
+
+describe('Set', () => {
+
+  it('add', () => {
+    
+  })
+
+  it('remove', () => {
+
+  })
+
+  it('has', () => {
+
+  })
+
+  it('intersection', () => {
+
+  })
+
+  it('union', () => {
+
+  })
+
+  it('difference', () => {
+
+  })
+
+  it('static intersection', () => {
+
+  })
+
+  it('static union', () => {
+
+  })
+  
+  it('static difference', () => {
+
+  })
+})
